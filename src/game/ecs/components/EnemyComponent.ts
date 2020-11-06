2e26cca3aa146c483414cda16572f1faa610ddf9
Dennis Meelker <dennis.meelker@infi.nl>
2020-11-06 14:05:01 +0100

    They need TP for their bungholes

--- src/game/ecs/components/EnemyComponent.ts
+++ src/game/ecs/components/EnemyComponent.ts
@@ -1,9 +1,15 @@
 import { Point } from "../../../utilities/Trig";
 import { Component, EntityId } from "../EntityComponentSystem";
 
+export enum EnemyState {
+    FindingTarget,
+    Leaving
+}
+
 export class EnemyComponent extends Component {
-    public target: Point;
-    
+    public state = EnemyState.FindingTarget;
+    public targetId: EntityId;
+
     constructor(entityId: EntityId) {
         super(entityId);
     }
--- src/game/ecs/systems/AISystem.ts
+++ src/game/ecs/systems/AISystem.ts
@@ -1,7 +1,10 @@
 import { Game } from "../../..";
-import { Vector } from "../../../utilities/Trig";
+import { Point, Vector } from "../../../utilities/Trig";
 import { GameState } from "../../GameState";
+import { DimensionsComponent } from "../components/DimensionsComponent";
+import { EnemyComponent, EnemyState } from "../components/EnemyComponent";
 import { EntityId } from "../EntityComponentSystem";
+import * as CarrierHelper from "./../utilities/CarrierHelper"
 
 const SPEED: number = 20;
 
@@ -10,17 +13,36 @@ export function update(game: Game) {
 
     for (let enemy of enemies) {
         const enemyDimensions = game.state.ecs.components.dimensionsComponents.get(enemy.entityId);
-        const playerDimensions = game.state.ecs.components.dimensionsComponents.get(game.state.playerId);
 
-        // TODO target toilet paper
-        enemy.target = playerDimensions.centerLocation;
+        switch(enemy.state) {
+            case EnemyState.FindingTarget:
+                if(!enemy.targetId || !targetVisible(game, enemy.targetId)) {
+                    enemy.targetId = findClosestTarget(game, enemyDimensions.centerLocation);
+                }
+        
+                const targetLocation = game.state.ecs.components.dimensionsComponents.get(enemy.targetId).centerLocation;
+        
+                if(targetReached(game, enemy, enemyDimensions)) {
+                    CarrierHelper.carryObject(game, enemy.entityId, enemy.targetId);
+                    enemy.state = EnemyState.Leaving;
+                } else {
+                    moveTowardsTarget(game, enemyDimensions, targetLocation);
+                }
+                break;
 
-        const velocity = enemy.target.subtract(enemyDimensions.centerLocation).toUnit()
-            .multiplyScalar(game.time.calculateMovement(SPEED));
-
-        if (canMove(game.state, enemy.entityId, velocity)) {
-            enemyDimensions.move(velocity);
+            case EnemyState.Leaving:
+                moveTowardsTarget(game, enemyDimensions, new Point(200, 400));
+                break;
         }
+    }
+}
+
+function moveTowardsTarget(game: Game, enemyDimensions: DimensionsComponent, targetLocation: Point) {
+    const velocity = targetLocation.subtract(enemyDimensions.centerLocation).toUnit()
+        .multiplyScalar(game.time.calculateMovement(SPEED));
+
+    if (canMove(game.state, enemyDimensions.entityId, velocity)) {
+        enemyDimensions.move(velocity);
     }
 }
 
@@ -34,4 +56,33 @@ export function canMove(state: GameState, id: EntityId, velocity: Vector): boole
     }
 
     return true;
+}
+
+function findClosestTarget(game: Game, ownLocation: Point): EntityId {
+    const allTargets = game.state.ecs.components.enemyTargetComponents.all;
+    let nearestTarget = null;
+
+    for(let target of allTargets) {
+        const targetLocation = game.state.ecs.components.dimensionsComponents.get(target.entityId).centerLocation;
+        const distance = ownLocation.distanceTo(targetLocation);
+
+        if(!nearestTarget || distance < nearestTarget.distance) {
+            nearestTarget = {
+                id: target.entityId,
+                distance: distance
+            };
+        }
+    }
+
+    return nearestTarget ? nearestTarget.id : null;
+}
+
+function targetReached(game: Game, enemy: EnemyComponent, enemyDimensions: DimensionsComponent): boolean {
+    const targetBounds = game.state.ecs.components.dimensionsComponents.get(enemy.targetId).bounds.addBorder(10);
+
+    return enemyDimensions.bounds.overlaps(targetBounds);
+}
+
+function targetVisible(game: Game, targetId: EntityId): boolean {
+    return game.state.ecs.components.dimensionsComponents.get(targetId) != null;
 }
