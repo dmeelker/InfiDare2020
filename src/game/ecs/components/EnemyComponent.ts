6dec4fd41418dadfb5b02dfe1175dedd2b4baa03
Wilco Kusee <wilcokusee@gmail.com>
2020-11-06 14:49:37 +0100

    Enemy wandering

--- src/game/ecs/components/EnemyComponent.ts
+++ src/game/ecs/components/EnemyComponent.ts
@@ -3,13 +3,14 @@ import { Component, EntityId } from "../EntityComponentSystem";
 
 export enum EnemyState {
     FindingTarget,
-    Leaving,
-    Wander
+    MovingToPos,
 }
 
 export class EnemyComponent extends Component {
     public state = EnemyState.FindingTarget;
+    public hasTP = false;
     public targetId: EntityId;
+    public targetPos: Point;
 
     constructor(entityId: EntityId) {
         super(entityId);
--- src/game/ecs/systems/AISystem.ts
+++ src/game/ecs/systems/AISystem.ts
@@ -3,53 +3,61 @@ import { Point, Vector } from "../../../utilities/Trig";
 import { GameState } from "../../GameState";
 import { DimensionsComponent } from "../components/DimensionsComponent";
 import { EnemyComponent, EnemyState } from "../components/EnemyComponent";
+import { LivingComponent } from "../components/LivingComponent";
 import { EntityId } from "../EntityComponentSystem";
 import * as CarrierHelper from "./../utilities/CarrierHelper"
 
-const SPEED: number = 20;
+const SPEED: number = 30;
 
 export function update(game: Game) {
     const enemies = game.state.ecs.components.enemyComponents.all;
 
     for (let enemy of enemies) {
-        const enemyDimensions = game.state.ecs.components.dimensionsComponents.get(enemy.entityId);
+        const enemyDimensions = game.state.ecs.components.dimensionsComponents.get(enemy.entityId) as LivingComponent;
 
-        switch(enemy.state) {
-            case EnemyState.FindingTarget:
-                if(!enemy.targetId || !targetVisible(game, enemy.targetId)) {
-                    enemy.targetId = findClosestTarget(game, enemyDimensions.centerLocation);
-
-                    if(!enemy.targetId) {
-                        continue;
-                    }
-                }
-        
-                const targetLocation = game.state.ecs.components.dimensionsComponents.get(enemy.targetId).centerLocation;
-        
-                if(targetReached(game, enemy, enemyDimensions)) {
-                    CarrierHelper.carryObject(game, enemy.entityId, enemy.targetId);
-                    enemy.state = EnemyState.Leaving;
+        switch (enemy.state) {
+            case EnemyState.MovingToPos:
+                if (enemy.targetPos.distanceTo(enemyDimensions.centerLocation) < 10) {
+                    enemy.targetPos = null;
+                    enemy.targetId = null;
+                    enemy.state = EnemyState.FindingTarget;
                 } else {
-                    moveTowardsTarget(game, enemyDimensions, targetLocation);
+                    moveTowardsTarget(game, enemy, enemyDimensions, enemy.targetPos);
                 }
                 break;
+            case EnemyState.FindingTarget:
+                if (enemy.hasTP) {
+                    moveTowardsTarget(game, enemy, enemyDimensions, new Point(200, 400));
+                } else {
+                    if (!enemy.targetId || !targetVisible(game, enemy.targetId)) {
+                        enemy.targetId = findClosestTarget(game, enemyDimensions.centerLocation);
+                    }
 
-            case EnemyState.Leaving:
-                moveTowardsTarget(game, enemyDimensions, new Point(200, 400));
-                break;
+                    const targetLocation = game.state.ecs.components.dimensionsComponents.get(enemy.targetId).centerLocation;
 
-            case EnemyState.Wander:
-                break;
+                    if (targetReached(game, enemy, enemyDimensions)) {
+                        CarrierHelper.carryObject(game, enemy.entityId, enemy.targetId);
+                        enemy.hasTP = true;
+                    } else {
+                        moveTowardsTarget(game, enemy, enemyDimensions, targetLocation);
+                    }
+                    break;
+                }
         }
     }
 }
 
-function moveTowardsTarget(game: Game, enemyDimensions: DimensionsComponent, targetLocation: Point) {
+function moveTowardsTarget(game: Game, enemy: EnemyComponent, enemyDimensions: DimensionsComponent, targetLocation: Point) {
+    const speed = enemy.state == EnemyState.MovingToPos ? SPEED + 10 : SPEED;
     const velocity = targetLocation.subtract(enemyDimensions.centerLocation).toUnit()
-        .multiplyScalar(game.time.calculateMovement(SPEED));
+        .multiplyScalar(game.time.calculateMovement(speed));
 
     if (canMove(game.state, enemyDimensions.entityId, velocity)) {
         enemyDimensions.move(velocity);
+    } else {
+        enemy.targetId = null;
+        enemy.targetPos = enemyDimensions.centerLocation.add(new Vector(50 - 100 * Math.random(), 50 - 100 * Math.random()))
+        enemy.state = EnemyState.MovingToPos;
     }
 }
 
@@ -69,11 +77,11 @@ function findClosestTarget(game: Game, ownLocation: Point): EntityId {
     const allTargets = game.state.ecs.components.enemyTargetComponents.all;
     let nearestTarget = null;
 
-    for(let target of allTargets) {
+    for (let target of allTargets) {
         const targetLocation = game.state.ecs.components.dimensionsComponents.get(target.entityId).centerLocation;
         const distance = ownLocation.distanceTo(targetLocation);
 
-        if(!nearestTarget || distance < nearestTarget.distance) {
+        if (!nearestTarget || distance < nearestTarget.distance) {
             nearestTarget = {
                 id: target.entityId,
                 distance: distance
